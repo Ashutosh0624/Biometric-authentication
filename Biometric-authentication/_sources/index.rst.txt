30cc731841f787c7bd13047794672ba50f7c190b
Ashutosh0624 <ashutosh.phd0602@gmail.com>
2025-01-12 17:23:28 +0530

    updated index.html

---
+++ Biometric-authentication/_sources/index.rst.txt
@@ -0,0 +1,15 @@
+Welcome to Biometric Authentication Using Facial Recognition's documentation!
+=============================================================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   abstract
+   introduction
+   literature_review
+   methodology
+   results
+   discussion
+   conclusion
+   references
--- Biometric-authentication/source/conf.py
+++ Biometric-authentication/source/conf.py
@@ -26,8 +26,9 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
+
 
 # -- Options for LaTeX output ------------------------------------------------
 # Settings for generating the paper in IEEE format
